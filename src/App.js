840e7903d2f8d0c95d40eb38e226d618d5dcbcfb
Aarush <aarush@gmail.com>
2024-05-02 02:53:44 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 const urlParams = new URLSearchParams(window.location.search);
 const limit = urlParams.get("limit") || 6;
-const q = urlParams.get("q") || 'showrecent';
 const App = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
   useEffect(() => {
-    fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}&q=${q}`)
+    console.log(searchQuery);
+    const q = searchQuery.replace(/ /g, "-");
+    if (q === "") {
+      return;
+    }
+    
+    fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-12?limit=${limit}&query=${q}`)
     .then((res) => {
         return res.json();
       })
@@ -19,13 +26,19 @@ const App = () => {
         }
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [searchQuery]);
+
+  const handleSearchChange = (event) => {    
+      setSearchQuery(event.target.value);
+  };
+
 
   return (
     <div>
+      <input style={{ width: "60%", boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)" }} type="text" onKeyDown={(event) => event.key === "Enter" && handleSearchChange(event)} placeholder="Search..." />
       {data.map((item) => (
         <div key={item.createdDateTime}>
-          <h3 style={{ color: "brown" }}> {item.createdDateTime} ---- model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
+          <h3 style={{ color: "brown" }}> model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
           <div style={{ border: "1px dotted black", padding: "2px" }}>
             <p style={{ color: "grey" }}>{item.question}</p>
           </div>
